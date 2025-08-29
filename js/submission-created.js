cda021bb0dde55cb92b9100887747644baec312c
Will Liu <willlltrue@gmail.com>
2025-08-29 19:07:04 +0800

    updated professional status in index.html

--- js/submission-created.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-    const payload = JSON.parse(event.body).payload;
-    // Custom logic here
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Form submission handled" })
-    };
-  };
-  
